f491529cd479d0925b29cbcb11071f4df6571a67
ucunurul <ucu.nurul.ulum@basajans.com>
2016-02-05 15:32:19 +0700

    New Project of System HR Oil Company with C#

---
+++ HROilCompany/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HROilCompany.Startup))]
+namespace HROilCompany
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
